ccc573fb611cc4be7fd7aed4276e2e13c22f5e3f
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-08-30 11:04:47 -0700

    functions and control statements

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.c>
+
+/**
+ * main - prints the hex values
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int array[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+		'A', 'B', 'C', 'D', 'E', 'F'};
+
+	for (n = 0; n < 16; n++)
+		putchar(array[n]);
+	putchar('\n');
+	return (0);
+}
