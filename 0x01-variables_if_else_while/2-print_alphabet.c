0f0fe9eadfaee81f6f6e20bf1bbb99062b0e7ad9
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-08-30 00:53:15 -0700

    variables and control statements

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lc
+ *
+ * Return: 0 
+ */
+int main(void)
+{
+	int i;
+	char alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l',
+				'm','n','o','p','q','r','s','t','u','v','w','x',
+				'y','z'};
+
+	for (i = 0; i < 26; i++)
+	       putchar(alphabet[i]);
+	Return (0);
+}	
