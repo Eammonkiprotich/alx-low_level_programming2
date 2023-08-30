52b65018c8e964a2e5c7b906159bfe0d20e8971c
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-08-30 10:58:27 -0700

    functions and control statements

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int n;
-	char array[26]={'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+	char array[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
 		'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
 		'x', 'y', 'z'};
 
