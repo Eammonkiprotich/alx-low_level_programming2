3af3893a46f64309847000a57ae185f815dff51c
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-08-30 01:26:41 -0700

    variables and control statements

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,15 +8,13 @@
 int main(void)
 {
 	int n;
-	char lower[26];
-	char upper[26];
-
-	lower = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+	char lower[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
 		'y', 'z'};
-	upper = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+	char upper[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
 		'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
 		'Y', 'Z'};
+
 	for (n = 0; n < 26; n++)
 		putchar(lower[n]);
 	for (n = 0; n < 26; n++)
