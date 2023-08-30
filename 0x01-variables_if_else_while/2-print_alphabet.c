276a243ad62d7851b43fd3a2a7c9f02b70c9421b
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-08-30 00:58:49 -0700

    variables and control statements

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 	int i;
-	char alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l',
-				'm','n','o','p','q','r','s','t','u','v','w','x',
-				'y','z'};
+	char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+				'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+				't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
 	for (i = 0; i < 26; i++)
 	       putchar(alphabet[i]);
