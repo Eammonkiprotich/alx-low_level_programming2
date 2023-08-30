1b0930b29d2bd8271b20684d78df0a0c9654a4a3
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-08-30 03:46:35 -0700

    variables and control statements

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints alphabet except q and e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	char lower[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		'y', 'z'};
+
+	for (n = 0; n < 26; n++)
+	{
+		if (lower[n] == q || lower[n] == e)
+			continue;
+		putchar(lower[n]);
+	}
+	putchar('\n');
+	return (0);
+}
