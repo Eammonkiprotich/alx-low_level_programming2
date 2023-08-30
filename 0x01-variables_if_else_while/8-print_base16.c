eca388432ecbc92638d452c8e42ba1b28cf31b93
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-08-30 11:11:54 -0700

    functions and control statements

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int n;
 	int array[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-		'A', 'B', 'C', 'D', 'E', 'F'};
+		'a', 'b', 'c', 'd', 'e', 'f'};
 
 	for (n = 0; n < 16; n++)
 		putchar(array[n]);
