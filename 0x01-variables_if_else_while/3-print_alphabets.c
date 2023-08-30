9a29ea96fb0df72d1cd4e323ce027da72fe07316
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-08-30 01:22:50 -0700

    variables and control statements

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lc and uppc
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	char lower[];
+	char upper[];
+
+	lower = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		'y', 'z'};
+	upper = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z'};
+	for (n = 0; n < 26; n++)
+		putchar(lower[n]);
+	for (n = 0; n < 26; n++)
+		putchar(upper[n]);
+	putchar('\n');
+	return (0);
+}
