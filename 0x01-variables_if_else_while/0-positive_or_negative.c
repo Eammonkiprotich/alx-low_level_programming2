c0f1443e85d2fc870127eb3c9417bae6a0cd35a7
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-08-30 00:07:26 -0700

    variables and control statements

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - prints wether a random num n is positive or negative
+ *
+ * Return: 0 
+ */
 int main(void)
 {
 	int n;
