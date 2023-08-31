5e39df977257eb7884d4e5039501872d7e380832
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-08-30 19:12:35 -0700

    variables

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible comb of 2 2-digit numbers
+ * main - prints combinations of 2 2-digit numbers
  *
  * Return: 0
  */
@@ -9,28 +9,21 @@ int main(void)
 {
 	int i;
 	int j;
-	int k;
-	int l;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			for (k = 0; k < 10; k++)
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i < 98 || j < 99)
 			{
-				for (l = 0; l < 10; l++)
-				{
-					if (i < j && j < k && k < l)
-					{
-						putchar(i + '0');
-						putchar(j + '0');
-						putchar(' ');
-						putchar(k + '0');
-						putchar(l + '0');
-						putchar(',');
-						putchar(' ');
-					}
-				}
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
