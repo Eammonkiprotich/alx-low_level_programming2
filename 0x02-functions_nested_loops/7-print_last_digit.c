5c818a4e796e569460a0e6ef6a8ecf0befe78dc0
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-01 00:30:00 -0700

    header

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/*
+ * print_last_digit - prints the last digit of a number
+ * @n: the number we extract the last digit from.
+ *
+ * Return: last digit of the number
+ */
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+}
