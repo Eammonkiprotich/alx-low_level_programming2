7cf561d6db01b916ffc075da9805f88ac380a233
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-02 00:29:57 -0700

    headers

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,13 +10,23 @@ void print_times_table(int n)
 	int j;
 	int prod;
 
-	for (i = 0; i <= n; i++)
+	if ( n <= 15 && n >= 0)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			prod = i * j;
-			_putchar(prod + '0');
-			_putchar(' ');
+			for (j = 0; j <= n; j++)
+			{
+				prod = i * j;
+				if (prod < 10)
+					_putchar(prod + '0');
+				else
+				{
+					_putchar((prod / 10) + '0');
+					_putchar((prod % 10) + '0');
+				}
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
 }
