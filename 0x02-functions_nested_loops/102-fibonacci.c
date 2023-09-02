eab0fec58101566d5c82a37086cb0ec3ee47305a
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-02 05:02:55 -0700

    headers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,18 +7,19 @@
  */
 int main(void)
 {
-	int num1 = 1;
-	int num2 = 2;
-	int num3;
+	long int num1 = 1;
+	long int num2 = 2;
+	long int num3;
 	int count = 0;
 
 	printf("%d, ", num1);
-	printf("%d, ", num2);
+	printf("%d", num2);
 
 	for (count = 0; count <48; count++)
 	{
+		printf(", ");
 		num3 = num2 + num1;
-		printf("%d, ", num3);
+		printf("%d", num3);
 		num1 = num2;
 		num2 = num3;
 	}
