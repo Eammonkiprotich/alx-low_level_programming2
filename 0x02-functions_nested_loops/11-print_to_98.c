f02fd99cb27a82a835e1724d4ea6cf21fd1413ee
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-01 23:06:00 -0700

    headers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,4 +3,23 @@
 /**
  * print_to_98 - prints all N numbers from n to 98
  * @n: the number
- *
+ */
+void print_to_98(int n)
+{
+	int i;
+	int j;
+
+	if (n < 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+			if (i < 10)
+				putchar(i + '0');
+			else
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+			}
+		}
+	}
+}
