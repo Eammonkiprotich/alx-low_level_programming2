d73bb1713b5da973bf664f9f312594f5efa84280
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-01 23:11:49 -0700

    headers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - prints all N numbers from n to 98
@@ -7,7 +8,7 @@
 void print_to_98(int n)
 {
 	int i;
-	int j;
+	/*bint j; */
 
 	if (n < 98)
 	{
