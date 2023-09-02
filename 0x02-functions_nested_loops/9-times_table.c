e0bf0a85e269149764e75f2f1a6af8d02f9c159b
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-02 02:10:54 -0700

    headers

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,24 +11,24 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+
+		for (j = 1; j <= 9; j++)
 		{
+			putchar(',');
+			putchar(' ');
+
 			n = i * j;
 
-			if (n < 10)
-			{
-				_putchar(' ');
-				_putchar(n + '0');
-			}
-			else
+			if (n >= 10)
 			{
 				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
 			}
-			if (j < 9)
+			else
 			{
-				_putchar(',');
 				_putchar(' ');
+				_putchar(n + '0');
 			}
 		}
 		_putchar('\n');
