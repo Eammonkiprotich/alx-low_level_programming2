bcd697b46875f42a6d6f71e2c703c3b14cadd6fe
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-03 04:18:20 -0700

    morefunctions

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints the \ n times
+ * @n: number of times the \ should be printed
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for(i = 1; i <= n; i++)
+	{
+		for( j = 0; j <= n; j++)
+			_putchar(' ')
+		_putchar('\');
+	}
+}
+			
