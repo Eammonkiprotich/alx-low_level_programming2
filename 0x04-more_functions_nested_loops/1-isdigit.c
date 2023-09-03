7e1be0b05b468ec8fc27d39f72748534739cef75
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-02 22:42:11 -0700

    morefunctions

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if c is a digit between 0 and 9
+ * @c: number to be checkedx
+ *
+ * Return: 1 if a digit 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
