1b843713b3cc7216d342738a144a4c7062d7642b
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-03 13:05:25 -0700

    morefunctions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,7 +18,11 @@ int main(void)
 		else if (i % 5 == 0)
 			printf("Buzz ");
 		else
-			printf("%d ", i);
+		{
+			if (i == 100)
+				printf("%d", i);
+			else
+				printf("%d ", i);
 	}
 	return (0);
 }
