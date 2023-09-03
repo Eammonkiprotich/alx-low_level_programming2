28188a06cc84d41e6bfc7f9201033da58022a3bb
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-03 01:59:22 -0700

    morefunctions

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line with n _ characters
+ * @n: number of characters to be printed
+ */
+void print_line(int n)
+{
+	int i;
+
+	for ( i = 1; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
