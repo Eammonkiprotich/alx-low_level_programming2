30b698e312180ed3d347ae55d18ef9f102f9bef4
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-04 05:11:39 -0700

    morefunc

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,9 +13,9 @@ void print_number(int n)
 		n = n * -1;
 	}
 
-	if (n >= 0 && <= 9)
+	if (n >= 0 && n <= 9)
 		_putchar(n + '0');
-	else if ( n >= 10 && <= 99)
+	else if ( n >= 10 && n <= 99)
 	{
 		_putchar((n / 10) + '0');
 		_putchar((n % 10) + '0');
