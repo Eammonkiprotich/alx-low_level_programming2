74e0cc4b5d9ecaa5be8d832d1e8e39f6a25cb37e
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-06 00:15:00 -0700

    malloc

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prints the string in reverse
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != 0)
+		i++;
+
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+}	
