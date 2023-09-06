0504c03f0cf4ad159c71b991789d145d98fa945e
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-06 02:02:05 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - prints string in reverse
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	i -= 1;
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+	}
+}
