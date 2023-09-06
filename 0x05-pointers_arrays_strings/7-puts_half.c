2d7af9adf1b35838e36c53fd1de2d121c3546a51
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-06 07:14:41 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half the string
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while(*(str + i))
+	{
+		i++;
+	}
+
+	if (i % 2 == 0)
+	{
+		i /=2;
+		while(*(str + i))
+		{
+			_putchar(*(str + i));
+			i++;
+		}
+	}
+	else
+	{
+		i = (i - 1) / 2;
+		while(*(str + i))
+		{
+			_putchar(*(str + i));
+			i++;
+		}
+	}
+}
