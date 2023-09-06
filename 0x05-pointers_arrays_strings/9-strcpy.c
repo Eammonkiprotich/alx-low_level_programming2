a045300ced400ef5fc79c36aeeb9f78aab0e33c5
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-06 09:22:58 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string from src to dst
+ * @dest: destination
+ * @src: source
+ *
+ * Return: value of the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while(*(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+
+	return (*(dest + 0));
+}
