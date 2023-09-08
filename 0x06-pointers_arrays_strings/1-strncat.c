99105b13434f47eccc1f34f30aa1cdb818556e4a
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-07 23:31:01 -0700

    morepointers

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - similar to _strcat but prints n bytes from src
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ *
+ * Return: pointer to string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*(dest + i))
+		i++;
+
+	while (n > 0)
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+		n--;
+	}
+	return (dest);
+}
