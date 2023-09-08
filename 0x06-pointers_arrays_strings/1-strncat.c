4f6830b98e2ad089f2634a32fe4655306bd48e40
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-08 01:10:13 -0700

    morepointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,19 +10,17 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len = 0;
-	int src_index = 0;
+	int i = 0;
+	int j = 0;
 
-	while (dest[dest_len])
-		dest_len++;
-
-	int i = dest_len;
-
-	while (n > 0 && src[src_index])
-	{
-		dest[i] = src[src_index];
+	while (*(dest + i))
 		i++;
-		src_index++;
+
+	while (n > 0)
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
 		n--;
 	}
 	return (dest);
