381c34bc0939f75b5b06c9116aa892fcab4e753b
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-08 10:33:53 -0700

    morepointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,3 +8,15 @@
  * Return: difference btwn the two values
  */
 int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int res = 0;
+
+	if (s1[i] & s2[i])
+	{
+		while ((s1[i] == s2[i]) && (s1[i] && s2[i]))
+			i++;
+		res = s1[i] - s2[i];
+	}
+	return (res);
+}
