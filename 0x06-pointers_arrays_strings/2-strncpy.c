16163d9db521dd28931df1456549e80c96e47548
Eammon <eammon.kiprotich21@students.dkut.ac.ke>
2023-09-08 03:03:55 -0700

    morepointers

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies a string
+ * @dest: destination
+ * @src: src string
+ * @n: number of bytes to be copied
+ *
+ * Return: pointer to the copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (*(src + i))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
